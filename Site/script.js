bdb4222dbacec93ff9123cb3f6599694b901eaae
Arthur314159 <113700564+Arthur314159@users.noreply.github.com>
2023-01-25 16:09:21 +0100

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -6,7 +6,6 @@ function OuvrirFichier() {
 
     reader.addEventListener("load", () => {
         content = reader.result;
-        lire();
     }, false);
 
     if (file) {
@@ -14,6 +13,22 @@ function OuvrirFichier() {
     } 
 }
 
-function lire() {
-    console.log(content [10]);
-}
+
+//function Questions( clef, enonce) {
+//    this.clef = clef;
+//    this.enonce = enonce;
+//}
+
+//function Reponses(clef, enonce) {
+//    this.clef = clef;
+//    this.enonce = enonce;
+//}
+//let question = new Questions('questions', '', '');
+
+//for (question of content) {
+//    console.log(question);
+//}
+
+const json = '{"id": "1", "enonce": "<h1>Quel est votre nom ?</h1>", "reponse": ["",""]}';
+const dico = JSON.parse(json);
+console.log(dico.enonce);
