f66cd880ffdbf663b2dd385f235b30246fc9b03c
Arthur314159 <113700564+Arthur314159@users.noreply.github.com>
2023-01-25 13:32:28 +0100

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -1,15 +1,19 @@
-
+var content;
 function OuvrirFichier() {
     const [file] = document.querySelector('input[type=file]').files;
     const reader = new FileReader();
 
 
     reader.addEventListener("load", () => {
-           
-     }, false);
+        content = reader.result;
+        lire();
+    }, false);
 
     if (file) {
         reader.readAsText(file);
-     }
+    } 
 }
 
+function lire() {
+    console.log(content [10]);
+}
