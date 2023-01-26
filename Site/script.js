7f69c53157f05b3c7382b0b7884277d6e2b50718
Arthur314159 <113700564+Arthur314159@users.noreply.github.com>
2023-01-26 13:48:14 +0100

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -1,34 +1,68 @@
-var content;
+var dico;
+var doc; 
+var bloc; 
+var sb; 
 function OuvrirFichier() {
     const [file] = document.querySelector('input[type=file]').files;
     const reader = new FileReader();
 
 
     reader.addEventListener("load", () => {
-        content = reader.result;
+        var content = reader.result;
+        dico = JSON.parse(content); //ouvre le fichier JSON
+        doc = dico.doc;
+        bloc = dico.bloc;
+        sb = dico.sb;
     }, false);
 
     if (file) {
         reader.readAsText(file);
-    } 
+    }
 }
 
 
-//function Questions( clef, enonce) {
-//    this.clef = clef;
-//    this.enonce = enonce;
-//}
 
-//function Reponses(clef, enonce) {
-//    this.clef = clef;
-//    this.enonce = enonce;
-//}
-//let question = new Questions('questions', '', '');
+function display_doc(doc)
+{
+    //Pour chaque bloc dans doc
+    call(display_bloc(idb));
+}
+function display_bloc(idb)
+{
+    var liste_sb = bloc[idb];
+    //Pour chaque sous-bloc dans liste_sb
+    call(display_sbloc(idsb));
+}
+function display_sbloc(idsb)
+{
+    var dic_sb_content = sb.idsb; // ou sb[idsb]
+    switch (type_sb) {
+        case texte:
+            call(display_texte);
+        case titre:
+            call(display_titre);
+        case QCM_1:
+            call(display_QCM_1);
+        case QCM_M:
+            call(display_QCM_M);
+    }
 
-//for (question of content) {
-//    console.log(question);
-//}
 
-const json = '{"id": "1", "enonce": "<h1>Quel est votre nom ?</h1>", "reponse": ["",""]}';
-const dico = JSON.parse(json);
-console.log(dico.enonce);
+}
+function display_texte(id2)
+{
+
+}
+function display_titre(texte)
+{
+
+}
+function display_QCM_1(question, reponse, FB, bonne_reponse)
+{
+
+}
+function display_QCM_M(question, reponse, FB, bonne_reponse)
+{
+
+}
+
