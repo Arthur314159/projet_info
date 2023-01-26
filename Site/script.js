7c0eed42eaacc5b76cfb8d36cf83a0b28cc75b8d
Arthur314159 <113700564+Arthur314159@users.noreply.github.com>
2023-01-26 18:16:29 +0100

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -13,6 +13,7 @@ function OuvrirFichier() {
         doc = dico.doc;
         bloc = dico.bloc;
         sb = dico.sb;
+        display_doc(doc);
     }, false);
 
     if (file) {
@@ -20,49 +21,86 @@ function OuvrirFichier() {
     }
 }
 
+function display_html_div(id_div, buffer_html) {
+    document.getElementById(id_div).innerHTML = buffer_html;
+}
 
+function add_html_div(id_div, buffer_html) {
+    document.getElementById(id_div).insertAdjacentHTML('afterend', buffer_html);
+}
 
 function display_doc(doc)
 {
-    //Pour chaque bloc dans doc
-    call(display_bloc(idb));
-}
-function display_bloc(idb)
-{
-    var liste_sb = bloc[idb];
-    //Pour chaque sous-bloc dans liste_sb
-    call(display_sbloc(idsb));
-}
-function display_sbloc(idsb)
-{
-    var dic_sb_content = sb.idsb; // ou sb[idsb]
-    switch (type_sb) {
-        case texte:
-            call(display_texte);
-        case titre:
-            call(display_titre);
-        case QCM_1:
-            call(display_QCM_1);
-        case QCM_M:
-            call(display_QCM_M);
+    for (var [pos,id_bloc] of Object.entries(doc)) {
+        let buffer_html = "<div id='" + id_bloc +"'></div>";
+        add_html_div("doc", buffer_html);
+        display_bloc(id_bloc);
     }
-
-
 }
-function display_texte(id2)
+function display_bloc(id_bloc)
 {
-
+    var buffer_html = "";
+    var liste_sb = bloc[id_bloc];
+    //console.log("liste_sb", liste_sb);
+    for (var [pos, id_sbloc] of Object.entries(liste_sb)) {
+        console.log("ici",pos, id_sbloc, buffer_html);
+        buffer_html = display_sbloc(id_bloc, id_sbloc, buffer_html);
+    }
+    console.log(buffer_html);
+    display_html_div(id_bloc, buffer_html);
 }
-function display_titre(texte)
+
+function display_sbloc(id_bloc,id_sbloc,buffer_html)
 {
+   // console.log(id_bloc, id_sbloc, buffer_html);
+    var type = sb[id_sbloc].type;
+    var content = sb[id_sbloc].content; // ou sb[idsb]
+    switch (type) {
+        case "texte":
+            buffer_html = display_texte(id_bloc, id_sbloc, content, buffer_html);
+            break;
+        case "titre":
+            buffer_html = display_titre(id_bloc, id_sbloc, content, buffer_html);
+            break;
+        case "QCM_1":
+            buffer_html = display_QCM_1(id_bloc, id_sbloc, content, buffer_html);
+            break;
+        case "QCM_M":
+            buffer_html = display_QCM_M(id_bloc, id_sbloc, content, buffer_html);
+            break;
+
+    }
+    return buffer_html;
 
 }
-function display_QCM_1(question, reponse, FB, bonne_reponse)
+function display_texte(id_bloc, id_sbloc, content, buffer_html)
 {
+    let texte = content.texte;
+    buffer_html += "<p> " + texte + "</p>";
+    return buffer_html;
 
 }
-function display_QCM_M(question, reponse, FB, bonne_reponse)
+
+function display_titre(id_bloc, id_sbloc, content, buffer_html)
 {
-
+    let titre = content.titre;
+    buffer_html += "<h1> " + titre + "</h1>";
+    return buffer_html;
 }
 
+function display_QCM_1(id_bloc, id_sbloc, content, buffer_html)
+{
+    let question = content.question;
+    let rep = content.reponse;
+    let FB = content.FB;
+    buffer_html += "<h3> " + question + "</h3>";
+    return buffer_html;
+}
+function display_QCM_M(id_bloc, id_sbloc, content, buffer_html)
+{
+    let question = content.question;
+    let rep = content.reponse;
+    let FB = content.FB;
+    buffer_html += "<h3> " + question + "</h3>";
+    return buffer_html;
+}
