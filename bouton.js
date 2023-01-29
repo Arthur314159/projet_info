6e6bdebd57ae5912283ee4087e2e75f5a84daa37
celia-d <113700996+celia-d@users.noreply.github.com>
2023-01-29 17:53:43 +0100

    Add files via upload

---
+++ bouton.js
@@ -0,0 +1,33 @@
+// // il faut créer une variable no_bouton = reponse[i]
+// function no_bouton(reponse){
+
+// }
+// function new_bouton_radio(id_bloc,id_sbloc,no_bouton,value) {
+//     let id_bouton = id_bloc + "_" + id_sbloc + "_" + no_bouton;
+//     var document.getElementById(id_bloc).innerHTML = value;
+//     // input type="radio" name="perso" id="perso1" value="sorciere" 
+//     // onclick="newstory2(value)"
+// }
+
+// function bouton_score(id_bloc,id_sbloc,nmbr_bouton,no_bouton,reponse) {
+//     //let id_bouton = id_bloc + "_" + id_sbloc + "_" + nmbr_bouton;
+//     //let type
+//     switch(type){
+
+//     case QCM_1 :
+//         for (let i = 0; i < reponse.lenght; i++)
+//         {
+//             if (i=document.getElementById(id_bouton).checked)
+//         }
+//     case QCM_M :
+//     default :
+//     }
+    
+//     document.getElementById(id_bloc).innerHTML = reponse;
+
+//     if (document.getElementById("").style.display = "block") {
+
+//     };
+//     // input type="radio" name="perso" id="perso1" value="sorciere" 
+//     // onclick="newstory2(value)"
+//}
