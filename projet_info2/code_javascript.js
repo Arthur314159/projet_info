981d2fc9097b23ea8ecaa9e3df5501d7141e45a0
Arthur314159 <113700564+Arthur314159@users.noreply.github.com>
2023-01-29 17:49:02 +0100

    Delete projet_info2 directory

--- projet_info2/code_javascript.js
+++
@@ -1,9 +0,0 @@
-fetch('texte.json')
-.then(response => response.json())
-.then(data => {
-    // utiliser les données ici
-    console.log(data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
--- projet_info2/python.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('texte.json') as jsonfile:
-   texte = json.load(jsonfile)
-questions = (texte["questions"])
-reponses = (texte["reponses"])
-print(questions)
-print(reponses)
